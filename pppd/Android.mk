6a842c80386e595eafaf57902f6942fe89f27fc8
Primiano Tucci <p.tucci@gmail.com>
2014-10-05 13:52:55 +0200

    Apply changes from udoo.org (UDOO_Android_4.3_Source_v2.0.tar.gz)

--- pppd/Android.mk
+++ pppd/Android.mk
@@ -30,8 +30,16 @@ LOCAL_SHARED_LIBRARIES := \
 LOCAL_C_INCLUDES := \
 	$(LOCAL_PATH)/include
 
-LOCAL_CFLAGS := -DANDROID_CHANGES -DCHAPMS=1 -DMPPE=1 -Iexternal/openssl/include
+# Force sizeof(enum)=4
+LOCAL_CFLAGS := -fno-short-enums -DANDROID_CHANGES -DCHAPMS=1 -DMPPE=1 -Iexternal/openssl/include
 
 LOCAL_MODULE:= pppd
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_EXECUTABLE)
 
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := chat.c
+LOCAL_SHARED_LIBRARIES := libcutils
+LOCAL_MODULE := chat
+LOCAL_MODULE_TAGS := optional
 include $(BUILD_EXECUTABLE)
